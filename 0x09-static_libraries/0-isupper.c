31bdd9701c689b6bf5990f24f945c6c0b6b14ce0
Joseph Mark-Inyah <jpmarkinyah@gmail.com>
2023-11-08 08:31:01 -0800

    lets seoe

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,3 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
-int _isupper(int c);
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,3 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-int _strlen(char *s);
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
