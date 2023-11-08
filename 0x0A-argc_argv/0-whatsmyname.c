bdc27172aa37d4e4b43acba53758ed34f1c3cf31
Joseph Mark-Inyah <jpmarkinyah@gmail.com>
2023-11-08 09:23:20 -0800

    hgfh

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints the name of program
- * @argc - number of arguments
- * @argv - vector of arguments
+ * @argc: number of arguments
+ * @argv: vector of arguments
  *
  * Return: 0
  */
@@ -13,5 +13,5 @@ int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	(void)argc;
-	return 0;
+	return (0);
 }
