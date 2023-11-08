02f8b87c6f29837a330bc1bd3bb82dc9d37346c4
Joseph Mark-Inyah <jpmarkinyah@gmail.com>
2023-11-08 09:50:07 -0800

    jj

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints number of argumnts
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
